1ffe4020026c98972094a261aec55d78d9afbdec
Colby Reichenbach <colbyrreichenbach@gmail.com>
2025-02-13 20:55:00 -0500

    Delete scripts directory

--- scripts/main.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-folder_name = Path("store")
-
-# Create folder if it does not exist
-folder_name.mkdir(parents=True, exist_ok=True)
-
-print(f"✅ Folder '{folder_name}' created successfully!")
